d12775fbde645f96b76737adfb6c1749e28b6dc1
Victorjbs97 <Victorjbs97@gmail.com>
2025-10-21 16:49:27 -0300

    Exercicios livros

---
+++ exercicios.php
@@ -0,0 +1,52 @@
+<?php 
+    require_once "./src/Livro.php";
+    $livro1 = new Livro();
+    $livro2 = new Livro();
+    $livro3 = new Livro();
+
+    $livro1->titulo = "o alienista";
+    $livro1->autor = "Machado de Assis";
+    $livro1->paginas = 74;
+
+    $livro2->titulo = "Neuromancer";
+    $livro2->autor = "William Gibson";
+    $livro2->paginas = 374;
+
+    $livro3->titulo = "Androides Sonham com Ovelhas Elétricas?";
+    $livro3->autor = "Philip K. Dick";
+    $livro3->paginas = 316;
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>Livros</title>
+</head>
+<body>
+    <section class="flex flex-row ...">
+
+        <h1 class="text-3xl font-bold underline text-sky-500 font-mono ...">Livros</h1>
+        <div class="border-4 bg-indigo-500 bg-clip-content p-3">
+            <hr>
+            <h2><?=$livro1->titulo?></h2>
+            <p><b><?=$livro1->paginas?></b> pg.</p>
+        </div>
+        
+        <div class="border-4 bg-indigo-500 bg-clip-content p-3">
+            <hr>
+            <h2><?=$livro2->titulo?></h2>
+            <p><b><?=$livro2->paginas?></b> pg.</p>
+        </div>
+        
+        <div class="border-4 bg-indigo-500 bg-clip-content p-3">
+            <hr>
+            <h2><?=$livro3->titulo?></h2>
+            <p><b><?=$livro3->paginas?></b> pg.</p>
+        </div>
+    </section>
+</body>
+</html>
---
+++ src/Livro.php
@@ -0,0 +1,10 @@
+<?php 
+
+    class Livro{
+        public string $titulo;
+        public string $autor;
+        public int $paginas;
+    }
+
+
+?>

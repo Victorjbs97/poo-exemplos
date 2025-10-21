2898fe3d0c1669e39786790aa1a03db9e8075508
Victorjbs97 <Victorjbs97@gmail.com>
2025-10-21 14:51:55 -0300

    commit

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
     $cliente1->nome = "Mônica";
     $cliente1->idade =25 ;
     $cliente1->email ="monica@email.com" ;
+
+    $cliente2->nome = "Ana";
+    $cliente2->idade = 22;
+    $cliente2->email = "ana@ana.com"
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -16,9 +20,22 @@
     <title>Exemplos</title>
 </head>
 <body>
-    <h1 style="color:red;background-color:yellow;font-size:2em;">Exemplos de PHP com POO</h1>
+    <h1 style="display:flex;color:red;background-color:yellow;font-size:2em; padding:20px; gap:20px">Exemplos de PHP com POO</h1>
     <hr>
     <h2>Trabalhando com classes e objetos</h2>
+    <h2>Acessando os dados do Objeto!</h2>
+    <h4>Cliente 1:</h4>
+    <ul>
+        <li><strong>Nome: </strong><?=$cliente1->nome?></li>
+        <li><strong>Idade: </strong><?=$cliente1->idade?></li>
+        <li><strong>Email: </strong><?=$cliente1->email?></li>
+    </ul>
+    <hr>
+    <section style="color:blue; font-size:2em">
+        <h4>Cliente B:</h4>
+        <p>O cliente <b><?=$cliente2->nome?></b> tem <b><?=$cliente2->idade?></b> e seu email é <b><?=$cliente2->email?></b>.</p>
+        <hr>
+    </section>
     <pre><?=var_dump($cliente1,$cliente2)?></pre>
 </body>
 </html>
--- src/Cliente.php
+++ src/Cliente.php
@@ -3,5 +3,13 @@
         public string $nome;
         public int $idade;
         public string $email;
+
+        public function mostrarDados(){
+            echo "<div>
+                    <h4>NOME DO CLIENTE</h4>
+                    <p><b>e-mail de contato:</b> EMAIL</p>
+                    <p><b>Idade: </b> IDADE anos</p>  
+                </div>";
+        }
     }
 ?>

a1f62522fb4e870cd16b954807ff2d593be4b9e3
Victorjbs97 <Victorjbs97@gmail.com>
2025-10-22 14:34:52 -0300

    __construct opcional

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
     $clienteA = new Cliente("João",27,"joao@email.com");
     $clienteB = new Cliente("Pedro",20,"pedro@email.com");
+    $clienteC = new Cliente("Marcos",36,"Marcos@email.com","11 951746767");
 
 ?>
 <!DOCTYPE html>
@@ -10,13 +11,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <title>Exemplos</title>
 </head>
-<body>
-    <h1>Exemplos de PHP com POO</h1>
+<body class="">
+    <h1 class="text-lg ...">Exemplos de PHP com POO</h1>
     <hr>
     <h2>Trabalhando com classes e construtor</h2>
 
-    <pre><?=var_dump($clienteA,$clienteB)?></pre>
+    <pre><?=var_dump($clienteA,$clienteB,$clienteC)?></pre>
 </body>
 </html>
--- src/Cliente.php
+++ src/Cliente.php
@@ -3,12 +3,17 @@
         public string $nome;
         public int $idade;
         public string $email;
-        /* Método construtur: responsavel por construir um objeto COM seus dados no mommento da criação */
 
-        public function __construct(string $valorDoNome, int $valorDaIdade, string $valorDoEmail ){
+
+        //Telefone é opcional, se não informado o valor é null.
+        public ?string $telefone; // ? indica que o atributo pode ser null 
+        /* Método construtur: responsavel por construir um objeto COM seus dados no mommento da criação */
+                                                                                                    //pode ser nulo.
+        public function __construct(string $valorDoNome, int $valorDaIdade, string $valorDoEmail, ?string $valorDoTelefone = null){
             $this->nome = $valorDoNome;
             $this->idade = $valorDaIdade;
             $this->email = $valorDoEmail;
+            $this->telefone = $valorDoTelefone;
         }
     }
 ?>
